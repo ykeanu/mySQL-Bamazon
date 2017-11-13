7154610137e145ca9cdeba095f812703e4916b5b
Ysrael K Hernandez <ysraelkhernandez@Ysraels-MacBook-Pro.local>
2017-11-12 17:20:05 -0800

    Solved

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,166 +5,101 @@ const inquirer = require('inquirer');
 // Formats SQL data
 require('console.table');
 
-// ==================== II. DECLARATIONS ====================
+// ==================== II. FUNCTIONS && DECLARATIONS ====================
+
+let currentQuantity = "";
+let selectedProduct = "";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "ice_creamDB"
+    host: "localhost",
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "",
+    database: "bamazon"
 });
 
-
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-        case "Find songs by artist":
-          artistSearch();
-          break;
-
-        case "Find all artists who appear more than once":
-          multiSearch();
-          break;
-
-        case "Find data within a specific range":
-          rangeSearch();
-          break;
-
-        case "Search for a specific song":
-          songSearch();
-          break;
-      }
+function displayItems() {
+    const query = 'SELECT * FROM Products';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
     });
+    promptPurchase();
 }
 
-
-function artistSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-        }
-        runSearch();
-      });
-    });
+function promptOptions() {
+    inquirer.prompt({
+            name: "task",
+            type: "list",
+            message: "How can we help you?",
+            choices: [
+                "Shop for Products",
+                "Leave Store"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.task) {
+                case "Shop for Products":
+                    displayItems();
+                    break;
+                case "Leave Store":
+                    console.log("Thank you for shopping with us");
+                    connection.end();
+                    break;
+            }
+        })
 }
 
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
-  });
+function promptPurchase() {
+    inquirer.prompt([{
+            name: "item",
+            type: "list",
+            message: "What would you like to purchase?",
+            choices: [
+                "MacBook",
+                "iPhone",
+                "Hololens",
+                "Basketball",
+                "Toothpaste",
+                "Hoodie",
+                "TennisBall",
+                "ToothBrush",
+                "24-packWater",
+                "GroundBeef"
+            ]
+        }, {
+            name: "quantity",
+            message: "How many would you like to purchase?"
+        }])
+        .then(function(answer) {
+            const query = "SELECT product_name, stock_quantity FROM products WHERE?";
+            connection.query(query, { product_name: answer.item }, function(err, res) {
+                if (err) throw err;
+                if (res[0].stock_quantity > answer.quantity) {
+                    console.log("Item is in stock!");
+                    currentQuantity = (res[0].stock_quantity) - (answer.quantity);
+                    selectedProduct = answer.item;
+                    const query = connection.query("UPDATE products SET ? WHERE?", [{
+                            stock_quantity: currentQuantity
+                        },
+                        {
+                            product_name: selectedProduct
+                        }
+                    ])
+                } else {
+                    console.log("Sorry, there aren't enough items in stock");
+                }
+                promptOptions();
+            });
+        })
 }
-
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
-
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
-    });
-}
-
-
 
 // ==================== III. MAIN PROCESS ====================
 
-inquirer.prompt([/* Pass your questions in here */]).then(answers => {
-    // Use user feedback for... whatever!!
-});
-
-
 
 connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+    if (err) throw err;
+    displayItems();
 });
--- schema.sql
+++ schema.sql
@@ -1,12 +1,30 @@
-CREATE database bamazon;
+DROP DATABASE IF EXISTS bamazon;
 
-USE bamazon; 
+CREATE database bamazon;
+USE bamazon;
 
 CREATE TABLE products (
-	item_id INT NOT NULL,
-	product_name VARCHAR(100) NULL,
-	department_name VARCHAR(100) NULL,
-	price DECIMAL(10,2) NULL,  -- 4 total numbers, decimal starts two from the last
-	stock_quantity INT NULL,
-	PRIMARY KEY (item_id)
+  position INT NOT NULL AUTO_INCREMENT,
+  item_id INT NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT,
+  UNIQUE (item_id),
+  PRIMARY KEY (position)
 );
+
+SELECT * FROM products;
+
+INSERT INTO products;
+VALUES
+  (1000, "MacBook", "Technology", 2000.00, 15),
+  (2345, "iPhone", "Technology", 300.00, 12),
+  (5234, "Hololens", "Technology", 500.00 21),
+  (8769, "Basketball", "Sports", 28.79, 54),
+  (1230, "Toothpaste", "Health", 11.99, 42),
+  (7937, "Hoodie", "Clothing", 18.99, 3),
+  (2409, "Tennis Ball", "Sports", 16.89, 15),
+  (5824, "Tooth Brush", "Health", 2.99, 134),
+  (4994, "24-pack Water", "Food", 1.99, 84),
+  (4708, "Ground Beef", "Food", 3.79, 16);

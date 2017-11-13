34503f37f491e156c24928e4251c588ef6649ab6
ykeanu <ykhernan@ucla.edu>
2017-11-12 16:03:25 -0800

    Additional examples

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,41 @@ const connection = mysql.createConnection({
 });
 
 
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
+
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
+
+        case "Search for a specific song":
+          songSearch();
+          break;
+      }
+    });
+}
+
+
 function artistSearch() {
   inquirer
     .prompt({
